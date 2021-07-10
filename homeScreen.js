ad565f092bb8c355f5fef82646e6e875357ef828
Maïté N Mac <ninamaite1@gmail.com>
2021-07-10 20:48:08 +0200

    init with webpack asyn funtion error

---
+++ homeScreen.js
@@ -0,0 +1,22 @@
+// import { getDatas } from '//index.js'
+// getDatas();
+// const HomeScreen = () => {
+//     render: () => {
+//         return `<article class="photographer__card">
+//         <a href="./src/pages/profilepage.html">
+//         <div class="photographer__id">
+//             <img src="./medias/Photographers_ID_Photos/${profile.portrait}">
+//             <h2>${profile.name}</h2>
+//         </div>
+//         <div class="photographer__detail">
+//             <h3>${profile.city}, ${profile.country}</h3>
+//             <p>${profile.tagline}</p>
+//             <p>${profile.price}€/jour</p>
+//         </div>
+//         </a>
+//         <div class="photographer__tags container__tags">
+//             ${tags(profile.tags)}
+//         </div>
+//     </article>`
+//     }
+// }
